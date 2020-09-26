6674980427cdd3c27e40ed3502992c0206a1820d
Preet Shah <shahpreetk@gmail.com>
2020-09-26 17:35:05 +0530

    customisable colour in accordion card components

--- mealguide-frontend/src/components/AccordionCard/AccordionCard.jsx
+++ mealguide-frontend/src/components/AccordionCard/AccordionCard.jsx
@@ -2,18 +2,21 @@ import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Accordion from 'react-bootstrap/Accordion'
 import Card from 'react-bootstrap/Card'
+import { IonCard, IonCardHeader } from '@ionic/react';
 
 
-const AccordionCard = ({title, body, eventKey, colour}) => {
+const AccordionCard = ({ title, body, eventKey, colour }) => {
     return (
-            <Card bg={colour} style={{ margin: '20px', border: 'none'}}>
-                <Accordion.Toggle style={{backgroundColor:colour}} as={Card.Header} eventKey={eventKey}>
-                    {title}
-                </Accordion.Toggle>
-                <Accordion.Collapse eventKey={eventKey}>
-                    <Card.Body>{body}</Card.Body>
-                </Accordion.Collapse>
-            </Card>
+        <IonCard color={colour}
+            style={{
+                margin: '20px', border: 'none', boxShadow: 'none', opacity:'60%'}}>
+            <Accordion.Toggle as={IonCardHeader} color='dark' style={{ backgroundColor: 'transparent', borderBottom: 'none' }} eventKey={eventKey}>
+                {title}
+            </Accordion.Toggle>
+            <Accordion.Collapse eventKey={eventKey}>
+                <Card.Body>{body}</Card.Body>
+            </Accordion.Collapse>
+        </IonCard>
     )
 }
 
