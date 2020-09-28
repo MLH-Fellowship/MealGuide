2e7fedce038c1f6358e11375c4ed0a02031b1786
Preet Shah <shahpreetk@gmail.com>
2020-09-29 00:48:36 +0530

    issue of google icon resolved

--- mealguide-frontend/src/components/SigninButton/SigninButton.jsx
+++ mealguide-frontend/src/components/SigninButton/SigninButton.jsx
@@ -1,15 +1,13 @@
-import { IonItem, IonLabel, IonChip, IonIcon} from '@ionic/react';
+import { IonLabel, IonChip, IonIcon} from '@ionic/react';
 import React from 'react';
 import {logoGoogle} from "ionicons/icons"
 
 const SigninButton = () => {
     return (
-        <IonItem class="UserSignin">
-            <IonChip>
-                <IonIcon icon="{logoGoogle}"/>
-                <IonLabel>SIGN IN WITH GOOGLE</IonLabel>
+            <IonChip style={{backgroundColor:'#A0E7E5'}}>
+                <IonIcon color='textgreen' icon={logoGoogle}/>
+                <IonLabel color='textgreen'>SIGN IN WITH GOOGLE</IonLabel>
             </IonChip>
-        </IonItem>
     )
 }
 
--- mealguide-frontend/src/pages/Profile.jsx
+++ mealguide-frontend/src/pages/Profile.jsx
@@ -17,17 +17,17 @@ const Profile = () => {
                 <LargeNavbar />
                 <Avatar />
                 <UserFact FactLabel="Points" FactValue="36 meals followed till date" />
-                <Highlight highlightsides='20px' height='3px'/>
+                <Highlight highlightsides='20px' highlighttop='0'/>
                 <UserFacts FactLableOne="Weight" FactValueOne="55 kg" FactLableTwo="Height"FactValueTwo="152 cm" FactLableThree="BMI" FactValueThree="20.24"/>
-                <Highlight highlightsides='20px' height='3px'/>
+                <Highlight highlightsides='20px' highlighttop='0'/>
                 <UserFacts FactLableOne="Age" FactValueOne="24" FactLableTwo="Gender"FactValueTwo="Female" FactLableThree="Preference" FactValueThree="Vegan"/>
-                <Highlight highlightsides='20px' height='3px'/>
+                <Highlight highlightsides='20px' highlighttop='0'/>
                 <UserFact FactLabel="College" FactValue="Harward University" />
-                <Highlight highlightsides='20px' height='3px'/>
+                <Highlight highlightsides='20px' highlighttop='0'/>
                 <UserFact FactLabel="No of Meals per day:" FactValue="Three" />
-                <Highlight highlightsides='20px' height='3px'/>
+                <Highlight highlightsides='20px' highlighttop='0'/>
                 <UserFact FactLabel="Health Goal" FactValue="Maintain Weight" />
-                <Highlight highlightsides='20px' height='3px'/>
+                <Highlight highlightsides='20px' highlighttop='0'/>
                 <SectionHeader SectionTitle="Specifications:"/>
                 <Slider sliderTitle='Carbohydrates:' sliderRangeMin='20' SliderRangeMax='2000' sliderColor="danger"/>
                 <Slider sliderTitle='Proteins:' sliderRangeMin='20' SliderRangeMax='2000' sliderColor="secondary"/>
