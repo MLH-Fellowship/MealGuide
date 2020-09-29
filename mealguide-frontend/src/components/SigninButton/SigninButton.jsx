3654c79b85185404dd78744d90dbbf8e9ad40b34
Farhan Kiyani <farhan2742@gmail.com>
2020-09-28 17:38:25 -0700

    Signin button fix
    Added google auth link to the signin button.

--- mealguide-frontend/src/components/SigninButton/SigninButton.jsx
+++ mealguide-frontend/src/components/SigninButton/SigninButton.jsx
@@ -6,10 +6,14 @@ import './SigninButton.css';
 
 const SigninButton = () => {
     return (
-            <IonChip style={{backgroundColor:'#A0E7E5'}} class="center SigninButton">
-                <IonIcon color='textgreen' icon={logoGoogle}/>
-                <IonLabel color='textgreen'>SIGN IN WITH GOOGLE</IonLabel>
-            </IonChip>
+        <IonChip style={{backgroundColor:'#A0E7E5'}} class="center SigninButton">
+            <IonIcon color='textgreen' icon={logoGoogle}/>
+            <IonLabel color='textgreen'>
+                <a class="signinLink" href="/auth/google">
+                    SIGN IN WITH GOOGLE
+                </a>
+            </IonLabel>
+        </IonChip>
     )
 }
 
