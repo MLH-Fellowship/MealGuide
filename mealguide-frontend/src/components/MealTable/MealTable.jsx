de3be2a95c05716536645400feae9311aaf3246b
Preet Shah <shahpreetk@gmail.com>
2020-09-30 01:29:17 +0530

    meals data loads from backend

--- mealguide-frontend/src/components/MealTable/MealTable.jsx
+++ mealguide-frontend/src/components/MealTable/MealTable.jsx
@@ -4,50 +4,46 @@ import Table from 'react-bootstrap/Table'
 import Highlight from '../Highlight/Highlight';
 // import './MealTable.css'
 
-const MealTable = ({}) => {
-    return (
-        <Table responsive hover borderless style={{ marginLeft: '20px' }}>
-            <thead>
-                <tr>
-                    <th></th>
-                    <th>Proteins (g)</th>
-                    <th>Carbs (g)</th>
-                    <th>Fats (g)</th>
-                    <th>Weight (g)</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>Eggs</td>
-                    <td>12</td>
-                    <td>0</td>
-                    <td>8</td>
-                    <td>50</td>
-                </tr>
-                <tr>
-                    <td style={{padding:'0', margin:'0'}}><Highlight height='2px'/></td>
-                    <td style={{padding:'0', margin:'0'}}><Highlight height='2px'/></td>
-                    <td style={{padding:'0', margin:'0'}}><Highlight height='2px'/></td>
-                    <td style={{padding:'0', margin:'0'}}><Highlight height='2px'/></td>
-                    <td style={{padding:'0', margin:'0'}}><Highlight height='2px'/></td>
-                </tr>
-                <tr>
-                    <td>Eggs</td>
-                    <td>12</td>
-                    <td>0</td>
-                    <td>8</td>
-                    <td>50</td>
-                </tr>
-                <tr>
-                    <td style={{padding:'0', margin:'0'}}><Highlight height='2px'/></td>
-                    <td style={{padding:'0', margin:'0'}}><Highlight height='2px'/></td>
-                    <td style={{padding:'0', margin:'0'}}><Highlight height='2px'/></td>
-                    <td style={{padding:'0', margin:'0'}}><Highlight height='2px'/></td>
-                    <td style={{padding:'0', margin:'0'}}><Highlight height='2px'/></td>
-                </tr>
-            </tbody>
-        </Table>
-    )
+const MealTable = ({ mealsOffered }) => {
+    console.log(mealsOffered)
+    return !mealsOffered.length ?
+        <h1>No Meals Found</h1> :
+        (<>
+            <Table responsive hover borderless style={{ marginLeft: '20px' }}>
+                <thead>
+                    <tr>
+                        <th></th>
+                        <th>Proteins (g)</th>
+                        <th>Carbs (g)</th>
+                        <th>Fats (g)</th>
+                        <th>Weight (g)</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        mealsOffered.map((user, i) => {
+                            return (
+                                <>
+                                    <tr>
+                                        <td>{mealsOffered[i].name}</td>
+                                        <td>{mealsOffered[i].nutrition.protein}</td>
+                                        <td>{mealsOffered[i].nutrition.carb}</td>
+                                        <td>{mealsOffered[i].nutrition.fat}</td>
+                                        <td>{mealsOffered[i].weight}</td>
+                                    </tr>
+                                    <tr>
+                                        <td style={{ padding: '0', margin: '0' }}><Highlight height='2px' /></td>
+                                        <td style={{ padding: '0', margin: '0' }}><Highlight height='2px' /></td>
+                                        <td style={{ padding: '0', margin: '0' }}><Highlight height='2px' /></td>
+                                        <td style={{ padding: '0', margin: '0' }}><Highlight height='2px' /></td>
+                                        <td style={{ padding: '0', margin: '0' }}><Highlight height='2px' /></td>
+                                    </tr>
+                                </>)
+                        })
+                    }
+                </tbody>
+            </Table> </>
+        )
 }
 
 export default MealTable;
--- mealguide-frontend/src/pages/Meals.jsx
+++ mealguide-frontend/src/pages/Meals.jsx
@@ -6,15 +6,13 @@ import MealTable from '../components/MealTable/MealTable';
 import meals from '../mealsdummy.json';
 
 const Meals = () => {
-    console.log(meals.mealsOffered)
-    console.log(window.location.pathname.split('/meals/')[1])
     return (
         <IonPage>
             <IonContent fullscreen>
                 <LargeNavbar />
                 <h3 style={{ marginLeft: '25px', marginTop: '20px' }}>{window.location.pathname.split('/meals/')[1]}</h3>
                 <Highlight highlightsides='20px' height='6px' />
-                <MealTable />
+                <MealTable mealsOffered={meals.mealsOffered}/>
             </IonContent>
         </IonPage>
     )
