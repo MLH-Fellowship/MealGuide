d0635c090818ed2d869052e400cda210ba46c61d
Preet Shah <shahpreetk@gmail.com>
2020-09-30 16:09:35 +0530

    back button added to meals page

--- mealguide-frontend/src/pages/Meals.jsx
+++ mealguide-frontend/src/pages/Meals.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import { IonContent, IonPage } from '@ionic/react';
+import { IonContent, IonPage, IonButton, IonIcon } from '@ionic/react';
+import {chevronBackOutline} from 'ionicons/icons';
 import LargeNavbar from '../components/Navbar/LargeNavbar';
 import Highlight from '../components/Highlight/Highlight';
 import MealTable from '../components/MealTable/MealTable';
@@ -10,7 +11,11 @@ const Meals = () => {
         <IonPage>
             <IonContent fullscreen>
                 <LargeNavbar />
-                <h3 style={{ marginLeft: '25px', marginTop: '20px' }}>{window.location.pathname.split('/meals/')[1]}</h3>
+                <div style={{display:'inline-flex'}}>
+                <IonButton fill='clear' href='/suggestions' style={{position: 'relative', margin:'15px 0 10px 20px'}}>
+                    <IonIcon color='dark' icon={chevronBackOutline} />
+                </IonButton>
+                <h3 style={{ marginLeft: '5px', marginTop: '20px', position: 'relative'}}>{window.location.pathname.split('/meals/')[1]}</h3></div>
                 <Highlight highlightsides='20px' height='6px' />
                 <MealTable mealsOffered={meals.mealsOffered}/>
             </IonContent>
