2d37377e10201731ceb7a3edaf8903a677ff603c
Preet Shah <shahpreetk@gmail.com>
2020-10-01 00:05:17 +0530

    changed hrefs to history push

--- mealguide-frontend/src/components/Navbar/LargeNavbar.jsx
+++ mealguide-frontend/src/components/Navbar/LargeNavbar.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import { IonButton, IonHeader, IonIcon, IonImg, IonTitle, IonToolbar } from '@ionic/react';
 import { personSharp, readerOutline } from 'ionicons/icons'
+import { useHistory } from "react-router-dom";
 import logo from '../../assets/logo.svg';
 import './Navbar.css';
 
 const Navbar = () => {
+    const history = useHistory();
+    const routeChange1 = () => {
+        let path = `/profile`;
+        history.push(path);
+    }
+    const routeChange2 = () => {
+        let path = `/suggestions`;
+        history.push(path);
+    }
     return (
         <IonHeader>
             <IonToolbar className="item-inner-custom">
@@ -13,10 +23,10 @@ const Navbar = () => {
                     <IonTitle className='appname'>MealGuide</IonTitle>
                     <IonTitle className='slogan'>Be Fit, Be Free</IonTitle>
                 </div>
-                <IonButton fill='clear' slot='end' className='navbutton' href='/profile'>
+                <IonButton fill='clear' slot='end' className='navbutton' onClick={routeChange1}>
                     <IonIcon color='dark' icon={personSharp} />
                 </IonButton>
-                <IonButton fill='clear' slot='end' className='navbutton' href='/suggestions'>
+                <IonButton fill='clear' slot='end' className='navbutton' onClick={routeChange2}>
                     <IonIcon color='dark' icon={readerOutline} />
                 </IonButton>
             </IonToolbar>
