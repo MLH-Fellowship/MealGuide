d5886b1dec4d9404c9604daae58addd52eea0c60
ridhambhat <ridham.bhat@gmail.com>
2020-10-01 01:42:09 +0530

    Environment variables set
    Removed file dependancy of id.js and added support for environment variables to make it deployment ready

--- api-src/routes/authRoute.js
+++ api-src/routes/authRoute.js
@@ -1,6 +1,5 @@
 const router = require('express').Router();
 const passport = require('passport');
-const keys = require('../config/ids');
 
 router.get('/auth/failed', (req,res) => res.send('Failed Log In'))
 router.get('/auth/success', (req,res) => res.send('Successful Log In '))
@@ -13,11 +12,10 @@ router.get('/auth/google',(req, res, next)=> {
 }));
 
 router.get('/auth/google/callback', 
-  passport.authenticate('google', {failureRedirect: keys.url.failureURL}),
+  passport.authenticate('google', {failureRedirect: process.env.failureURL}),
   function(req, res) {
     console.log(req.user);
-    res.redirect(keys.url.successURL+'?email='+req.user._json.email);
+    res.redirect(process.env.successURL+'?email='+req.user._json.email);
 });
-
 
 module.exports = router;
