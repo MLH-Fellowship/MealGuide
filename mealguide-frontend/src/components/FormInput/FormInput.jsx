3676fc61f5eb01cf300cd074f5442d8223ca027b
Farhan Kiyani <farhan2742@gmail.com>
2020-09-30 18:40:29 -0700

    Form Input Component
    Made an component to take user's input for update profile form

---
+++ mealguide-frontend/src/components/FormInput/FormInput.jsx
@@ -0,0 +1,27 @@
+import { IonIcon } from '@ionic/react';
+import React from 'react';
+import Form from 'react-bootstrap/Form';
+import InputGroup from 'react-bootstrap/InputGroup'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './FormInput.css'
+
+const FormInput = ({name, Type, placeHolder, validFeedback, invalideFeedback, helpText, Value, icon}) => {
+    return (
+        <Form.Group id={"form__" + name} class="form-group">
+            <Form.Label for={name} class="form__label sr-only sr-only-focusable">
+                {name}:
+            </Form.Label>
+            <InputGroup>
+                <div class="input-group-prepend">
+                    <span class="input-group-text" id="basic-addon1"><IonIcon icon={icon} /></span>
+                </div>
+                <Form.Control type={Type} name={name} id={name} placeholder={placeHolder} class="form-control check__validation" defaultValue={Value} required />
+            </InputGroup>
+            <Form.Control.Feedback type="valid">{validFeedback}</Form.Control.Feedback>
+            <Form.Control.Feedback type="invalid">{invalideFeedback}</Form.Control.Feedback>
+            <Form.Text dark>{helpText}</Form.Text>
+        </Form.Group>
+    )
+}
+
+export default FormInput;
