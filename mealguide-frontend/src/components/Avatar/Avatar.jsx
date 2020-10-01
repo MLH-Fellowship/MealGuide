5016617d750a88c18e420842b2d5f68e12c2c9e3
Farhan Kiyani <farhan2742@gmail.com>
2020-09-30 20:04:21 -0700

    Edit Profile Link Fix
    Fixed the routing issue on the update profile link.

--- mealguide-frontend/src/components/Avatar/Avatar.jsx
+++ mealguide-frontend/src/components/Avatar/Avatar.jsx
@@ -1,8 +1,14 @@
 import { IonAvatar, IonItem, IonLabel, IonChip} from '@ionic/react';
 import React from 'react';
-import './Avatar.css'
+import './Avatar.css';
+import { useHistory } from "react-router-dom";
 
 const Avatar = ({username}) => {
+    const history = useHistory();
+    const routeChange = () => {
+        let path = `/editProfile`;
+        history.push(path);
+    }
     return (
         <IonItem class="UserProfile">
             <IonAvatar slot="start" class="userProfileImage">
@@ -11,7 +17,11 @@ const Avatar = ({username}) => {
             <IonLabel>
                 <h2 class="userProfileName">{username}</h2>
                 <IonChip class="ion-margin-start userProfileLink">
-                    <ion-label class="">Edit Profile</ion-label>
+                    <ion-label class="">
+                        <a class="editProfileLink" onClick={routeChange}>
+                            Edit Profile
+                        </a>
+                    </ion-label>
                 </IonChip>
             </IonLabel>
         </IonItem>
