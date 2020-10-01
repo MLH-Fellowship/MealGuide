d8da1148998a7639da761096fc7b8bdf9bf83bee
Farhan Kiyani <farhan2742@gmail.com>
2020-09-30 18:42:50 -0700

    Form Select Component
    Created a form select component on which user can select an option from a drop down list to be used in the Update Profile Page.

---
+++ mealguide-frontend/src/components/FormSelect/FormSelect.jsx
@@ -0,0 +1,31 @@
+import { IonIcon } from '@ionic/react';
+import React from 'react';
+import Form from 'react-bootstrap/Form';
+import InputGroup from 'react-bootstrap/InputGroup'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './FormSelect.css'
+
+const FormInput = ({name, Options, Value, validFeedback, invalideFeedback, helpText, icon}) => {
+    return (
+        <Form.Group id={"form__" + name} class="form-group">
+            <Form.Label for={name} class="form__label sr-only sr-only-focusable">
+                {name}:
+            </Form.Label>
+            <InputGroup>
+                <div class="input-group-prepend">
+                    <span class="input-group-text" id="basic-addon1"><IonIcon icon={icon} /></span>
+                </div>
+                <Form.Control as="select">
+                {Options.map(option => (
+                    option == Value ? <option selected>{option}</option> : <option>{option}</option>
+                ))}
+                </Form.Control>
+            </InputGroup>
+            <Form.Control.Feedback type="valid">{validFeedback}</Form.Control.Feedback>
+            <Form.Control.Feedback type="invalid">{invalideFeedback}</Form.Control.Feedback>
+            <Form.Text dark>{helpText}</Form.Text>
+        </Form.Group>
+    )
+}
+
+export default FormInput;
