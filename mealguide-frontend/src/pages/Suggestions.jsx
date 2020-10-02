4cd6dd4ff7ba0b2137663300b491b12b79943e83
Farhan Kiyani <farhan2742@gmail.com>
2020-10-02 03:20:48 -0700

    Minor fix

--- mealguide-frontend/src/pages/Suggestions.jsx
+++ mealguide-frontend/src/pages/Suggestions.jsx
@@ -1,23 +1,22 @@
 import React from 'react';
 import { IonContent, IonPage } from '@ionic/react';
+// import SmallNavbar from '../components/Navbar/SmallNavbar';
 import LargeNavbar from '../components/Navbar/LargeNavbar';
 import AccordionCard from '../components/AccordionCard/AccordionCard';
-import saveLocal from '../helpers/saveLocal';
+import Accordion from 'react-bootstrap/esm/Accordion';
+
 const Suggestions = () => {
-    saveLocal("ridham.bhat@gmail.com","https://mealguide.herokuapp.com/api/user/getUser ","users");
-    saveLocal("ridham.bhat@gmail.com","https://mealguide.herokuapp.com/api/user/getRecommendations ","recomendations");
     return (
         <IonPage>
             {/* <SmallNavbar /> */}
             <IonContent fullscreen>
                 <LargeNavbar />
-                {/* <Accordion> */}
-                <h3 style={{marginLeft:'25px'}}>Suggestions</h3>
-                <AccordionCard title='Breakfast' subtitle='Heavy' colour='pastelpink' body='This is the body' id='Breakfast'/>
-                <AccordionCard title='Lunch' subtitle='Light' colour='pastelblue' body='This is the body' id='Lunch'/>
-                <AccordionCard title='Snacks' subtitle='Heavy' colour='pastelgreen' body='This is the body' id='Snacks'/>
-                <AccordionCard title='Dinner' subtitle='Light' colour='pastelyellow' body='This is the body' id='Dinner'/>
-                {/* </Accordion> */}
+                <Accordion>
+                    <AccordionCard title='Breakfast' subtitle='Heavy' colour='pastelpink' body='This is the body' eventKey='0'/>
+                    <AccordionCard title='Lunch' subtitle='Light' colour='pastelblue' body='This is the body' eventKey='1'/>
+                    <AccordionCard title='Snacks' subtitle='Heavy' colour='pastelgreen' body='This is the body' eventKey='2'/>
+                    <AccordionCard title='Dinner' subtitle='Light' colour='pastelyellow' body='This is the body' eventKey='3'/>
+                </Accordion>
             </IonContent>
         </IonPage>
     )
